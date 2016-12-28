d66630585bce25097db1984cc14881ef0f45426a
Etudiant <Etudiant>
2016-12-28 14:16:40 +0100

    chanjeman

--- Maquette/js/master.js
+++ Maquette/js/master.js
@@ -14,17 +14,36 @@ $(function (){
   });
 
   $('input[type="checkbox"]').on("click", function () {
+    // On met dans une variable la valeur de l'input
     var liste = $(this).val();
-    var text = $(this).parents().Text();
+    // On récupère le nom de l'input
+    var text = $(this).attr("name");
+    // On vérifie si la checkbox est checkée
     if ($(this).prop('checked')) {
+      // Si oui, on regarde s'il existe déjà un li avec le [name] de l'input
       if ($('li').filter('.' + liste).length) {
+        // Si oui on fait rien
       } else {
+        // Sinon on en crée un
         $('<li>').html(text).appendTo('#basket').addClass(liste);
       }
+      // Si l'input n'est pas check:
     } else {
+      // On regarde s'il existe un li avec le [name]
       if ($('li').filter('.' + liste).length) {
+        // Si oui on le supprime.
         $('.' + liste).remove();
       }
     }
   });
+
+  function isEmpty (el) {
+    return !$.trim(el.html())
+ }
+
+  if (isEmpty($('#basket'))) {
+    $('#basket').addClass('none')
+  } else {
+    $('#basket').removeClass('.none')
+  }
 });
