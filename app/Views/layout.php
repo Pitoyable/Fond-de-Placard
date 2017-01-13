6d06b59846619151e37bddda8472b64563368939
bacaAlexandre <Alexandre76b@aol.com>
2017-01-13 14:14:01 +0100

    correction routes

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -12,11 +12,11 @@
 			<h1>W :: <?= $this->e($title) ?></h1>
 		</header>
 		<nav>
-			<p><a href="recipe_afficher/">J'ai faim</a></p>
-			<p><a href="theme_afficher/">Soirée</a></p>
+			<p><a href="recipe_afficher">J'ai faim</a></p>
+			<p><a href="theme_afficher">Soirée</a></p>
 			<p><a href="#">Se fournir</a></p>
 			<p><a href="#">Nous contacter</a></p>
-			<p><a href="user_update/">Mon compte</a></p>
+			<p><a href="user_update">Mon compte</a></p>
 		</nav>
 		<section>
 			<?= $this->section('main_content') ?>
--- app/routes.php
+++ app/routes.php
@@ -4,17 +4,17 @@
 		['GET', '/', 'Default#home', 'default_home'],
 
 		//user
-		['GET|POST', '/user_signIn/', 'User#signIn', 'User_signIn'],//inscription
-		['GET|POST', '/user_login/', 'User#login', 'User_login'],//connexion
-		['GET|POST', '/user_logout/', 'User#logout', 'User_logout'],//déconnexion
-		['GET|POST', '/user_update/', 'User#update', 'User_update'],//mise à jour
-		['GET|POST', '/user_valide/', 'User#valide', 'User_valide'],//valider email
+		['GET|POST', '/user_signIn', 'User#signIn', 'User_signIn'],//inscription
+		['GET|POST', '/user_login', 'User#login', 'User_login'],//connexion
+		['GET|POST', '/user_logout', 'User#logout', 'User_logout'],//déconnexion
+		['GET|POST', '/user_update', 'User#update', 'User_update'],//mise à jour
+		['GET|POST', '/user_valide', 'User#valide', 'User_valide'],//valider email
 
 		//recipe
-		['GET|POST', '/recipe_afficher/', 'recipe#afficher', 'recipe_afficher'],// montrer
-		['GET|POST', '/recipe_update/', 'recipe#update', 'recipe_update'],//mise à jour
-		['GET|POST', '/recipe_written/', 'recipe#written', 'recipe_written'],// crée
+		['GET|POST', '/recipe_afficher', 'recipe#afficher', 'recipe_afficher'],// montrer
+		['GET|POST', '/recipe_update', 'recipe#update', 'recipe_update'],//mise à jour
+		['GET|POST', '/recipe_written', 'recipe#written', 'recipe_written'],// crée
 
 		//trader_ht_trendmode
-			['GET|POST', '/theme_afficher/', 'theme#afficher', 'theme_afficher'],// montrer
+			['GET|POST', '/theme_afficher', 'theme#afficher', 'theme_afficher'],// montrer
 	);
