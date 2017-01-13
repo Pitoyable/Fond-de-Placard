4914e32aee3c8ffd4d5252c5f75ded4caf9a0cd7
ler-theo <lerquet.theo@orange.fr>
2017-01-13 14:13:31 +0100

    mdp change

--- BDD/initBDD/script.php
+++ BDD/initBDD/script.php
@@ -1,7 +1,7 @@
 <?php
 //Connect to BDD
 try {
-  $bdd = new PDO("mysql:host=localhost", "root", "123456",
+  $bdd = new PDO("mysql:host=localhost", "root", "",
   array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8')
 );
 } catch (Exception $e) {
