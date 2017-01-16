ae2bac3cce8329b955bc2f0bb4994852782501b9
Pitoyable <goyerpauld@gmail.com>
2017-01-16 16:35:44 +0100

    Admin gestion des utilisateurs

--- app/Views/administration/admin_user.php
+++ app/Views/administration/admin_user.php
@@ -2,4 +2,26 @@
 
 <?php $this->start('main_content') ?>
 <h1>Gestion des utilisateurs</h1>
+
+<table>
+  <tr>
+    <th>Pseudo</th>
+    <th>Mot de passe</th>
+    <th>Email</th>
+    <th>Valide</th>
+    <th>Groupe</th>
+    <th>Actions</th>
+  </tr>
+  <!-- Boucle sur les utilisateurs -->
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <!-- A revoir la pertinence des boutons -->
+    <td><button type="submit" name="modif">Modifier</button>
+        <button type="submit" name="delete">Supprimer</button></td>
+  </tr>
+</table>
 <?php $this->stop('main_content') ?>
