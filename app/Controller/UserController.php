ba4eba9c01f962d39da6982b98a17bdc640dbb9d
bacaAlexandre <Alexandre76b@aol.com>
2017-01-17 09:36:11 +0100

    methode user

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -13,28 +13,40 @@ class UserController extends Controller
   public function signUp(){
     //methode pour s'inscrire
     var_dump($_POST);
-    // methode poru hasher le mdp
-    // $modelHash = new AuthentificationModel();
-    // $passwordHash = $modelHash -> hashPassword($_POST['password']);
-    $arrayData = array(
-      'use_pseudo' => $_POST['pseudo'],
-      'use_email' => $_POST['email'],
-      'use_password' => $_POST['password'],
-    );
-    $model = new UsersModel();
-    $truc = $model -> insert($arrayData, $stripTags = true);
-    //problems erreur !!!!!
-    var_dump($truc);
+    $usersModel = new UsersModel();
+    if ($usersModel -> usernameExists($_POST['pseudo']) || $usersModel ->emailExists($_POST['email'])) {
+      echo "pseudo ou email existe déja";
+    }else{
+      // methode pour hasher le mdp
+      $modelHash = new AuthentificationModel();
+      $passwordHash = $modelHash -> hashPassword($_POST['password']);
+      $arrayData = array(
+        'use_pseudo' => $_POST['pseudo'],
+        'use_email' => $_POST['email'],
+        'use_password' => $passwordHash,
+      );
+      $model = new UsersModel();
+      $truc = $model -> insert($arrayData, $stripTags = true);
+      //problems erreur avec la methode find() pas d'id mais je ne comprend pas pourquoi il utilise find ...!!!!!
+      var_dump($truc);
+      echo"good";
+    }
+
+
   }
   public function login(){
     //methode pour se connecté
     $authentification = new AuthentificationModel();
     $test = $authentification -> isValidLoginInfo($_POST['pseudo'], $_POST['password']);
 		//$authentification -> logUserIn($_POST);
-    echo 'good';
-    var_dump($_SESSION);
     var_dump($test);
-    // a finir apres avoir reussi l'inscription pour le mdp hasher
+    if ($test == 0){
+      echo"erreur";
+    }else{
+      echo 'good';
+      $authentification ->logUserIn($test);
+      }
+    var_dump($_SESSION);
 
 
 
