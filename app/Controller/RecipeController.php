1d6eb58f0902e95ac4a9545167c3049f462d117b
ler-theo <lerquet.theo@orange.fr>
2017-01-17 16:26:47 +0100

    Debut sur la partie recipe, Method et Model, probleme a regler sur les tableaux

--- app/Controller/RecipeController.php
+++ app/Controller/RecipeController.php
@@ -7,24 +7,22 @@ use Model\RecipeModel;
 
 
 
-class RecipeController extends Controller
+class RecipeController extends \W\Controller\Controller
 {
+
   public function display(){
 
-    //Verification qu'un $_POST est envoyer
     if (!empty($_POST['search_bar'])) {
-      echo 'bonjour';
-      var_dump($_POST);
-      var_dump($_SESSION);
 
-      //Instance du model Recipe
-      $recipeModel = new RecipeModel();
+      //Instance du Model RecipeModel
+      $modelRecipe = new RecipeModel();
 
-      //utilisation de la method findRecipe
-      $recipeModel -> findRecipe();
+      //Utilisation de la method findIngredient
+      $modelRecipe -> findIngredient();
 
     }
 
+    //Affichage de la page
     $this->show('recipe/recipe_display');
 
   }
--- app/Model/RecipeModel.php
+++ app/Model/RecipeModel.php
@@ -6,13 +6,35 @@ use \W\Model\Model;
 class RecipeModel extends \W\Model\Model
 {
 
-  public function findRecipe() {
+  //Function pour l'auto complementation des ingredients (AJAX)
+  public function autoFindIngredient() {
 
-    if (!empty($_SESSION['ingredients'])) {
-      $_SESSION['ingredients'] = array(
-        "ingredients" => $_POST['search_bar'],
-      );
-    }
+  }
+
+  //Trouver l'ingredients rechercher et stockage dans $_SESSION['ingredients']
+  public function findIngredient() {
+
+    //Instance du Model RecipeModel Pour avoir accés au method de Model
+    $model = new RecipeModel();
+
+
+    //Creation d'un tableau pour la method search();
+    $array = array(
+      "ing_name" => $_POST['search_bar'],
+    );
+
+    //Utilisation de la method setTable() pour chercher dans la table ingredients
+    $model -> setTable('ingredients');
+
+    //Utilisation de la method search()
+    $ingFind = $model -> search($array);
+
+    //return du resultat
+    return $ingFind;
+  }
+
+  //Trouver les recettes avec les conditions ($_SESSION['ingredients'], type)
+  public function findRecipe() {
 
 
   }
