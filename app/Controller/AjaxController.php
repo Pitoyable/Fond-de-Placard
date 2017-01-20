323e8ec8ba31fb84c6e2a6ee9ee0928eb87705b1
ler-theo <lerquet.theo@orange.fr>
2017-01-20 14:32:36 +0100

    Block Ajax

--- app/Controller/AjaxController.php
+++ app/Controller/AjaxController.php
@@ -8,35 +8,13 @@ class AjaxController
 
   public function findIngredient() {
 
-    // $credentials = array(
-    //   "ingredient" => $_POST['search_bar']
-    // );
-    //
-    //
-    // //Partie pour trouver l'ingredients en BDD
-    // if (!empty($credentials['ingredient'])) {
-    //
-    //   //Instance du Model RecipeModel
-    //   $modelRecipe = new RecipeModel();
-    //
-    //   //Appel de la method findIngredient et recuperation du resultat
-    //   $ingFind = $modelRecipe -> findIngredientBdd();
-    //
-    //
-    //
-    //
-    // }
 
-    //Je renvois une reponse JSON
     header('Content-Type: application/json');
-
     //Je formate la reponse en JSON
     echo json_encode(array(
       "success" => true,
-      "bisous" => 'bisous'
+      "test" => 'bisous'
     ));
-
-
 
   }
 
